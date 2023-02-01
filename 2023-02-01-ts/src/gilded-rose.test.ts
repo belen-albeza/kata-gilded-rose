9e36a7905802d2d7b71ed587315f67db2e375c91
Belén Albeza <belen@hey.com>
2023-02-01 22:50:49 +0100

    add more snapshot tests

--- 2023-02-01-ts/src/gilded-rose.test.ts
+++ 2023-02-01-ts/src/gilded-rose.test.ts
@@ -5,4 +5,19 @@ describe("Gilded Rose golden master tests", () => {
     const goldenMaster = runGoldenMaster(1);
     expect(goldenMaster).toMatchSnapshot();
   });
+
+  it("Matches the golden master after 2 days", () => {
+    const goldenMaster = runGoldenMaster(2);
+    expect(goldenMaster).toMatchSnapshot();
+  });
+
+  it("Matches the golden master after 5 days", () => {
+    const goldenMaster = runGoldenMaster(5);
+    expect(goldenMaster).toMatchSnapshot();
+  });
+
+  it("Matches the golden master after 10 days", () => {
+    const goldenMaster = runGoldenMaster(10);
+    expect(goldenMaster).toMatchSnapshot();
+  });
 });
