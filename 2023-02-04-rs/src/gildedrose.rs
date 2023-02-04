d387372f286972f3f09f5175caedb97a2316b961
Belén Albeza <belen@hey.com>
2023-02-04 17:48:34 +0100

    extracted the logic to update a single item

--- 2023-02-04-rs/src/gildedrose.rs
+++ 2023-02-04-rs/src/gildedrose.rs
@@ -36,56 +36,61 @@ impl GildedRose {
     }
 
     pub fn update_quality(&mut self) {
-        for item in &mut self.items {
-            if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-            {
-                if item.quality > 0 {
-                    if item.name != "Sulfuras, Hand of Ragnaros" {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1;
+        self.items = self.items.iter().map(|x| self.update_item(x)).collect()
+    }
 
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+    fn update_item(&self, original_item: &Item) -> Item {
+        let mut item = original_item.clone();
 
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
+        if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality = item.quality - 1;
                 }
             }
+        } else {
+            if item.quality < 50 {
+                item.quality = item.quality + 1;
 
-            if item.name != "Sulfuras, Hand of Ragnaros" {
-                item.sell_in = item.sell_in - 1;
-            }
-
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
-                                item.quality = item.quality - 1;
-                            }
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.sell_in < 11 {
+                        if item.quality < 50 {
+                            item.quality = item.quality + 1;
                         }
-                    } else {
-                        item.quality = item.quality - item.quality;
                     }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
+
+                    if item.sell_in < 6 {
+                        if item.quality < 50 {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
             }
         }
+
+        if item.name != "Sulfuras, Hand of Ragnaros" {
+            item.sell_in = item.sell_in - 1;
+        }
+
+        if item.sell_in < 0 {
+            if item.name != "Aged Brie" {
+                if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.quality > 0 {
+                        if item.name != "Sulfuras, Hand of Ragnaros" {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality;
+                }
+            } else {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
+        item
     }
 }
 
