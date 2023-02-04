25abdd7fe06f3990a6e8f1fdba43b8f6a38aaa5a
Belén Albeza <belen-albeza@users.noreply.github.com>
2023-02-04 10:30:07 +0100

    2023-02-01 TypeScript (#2)
    * add more snapshot tests
    
    * update readme files
    
    * add instructions
    
    * extract processing single item into separate function
    
    * fix snapshots so tests are idempotent
    
    * make the single processing item function not to modify the item its passed, but to return a copy
    
    * implement item kinds
    
    * use kinds instead of hardcoded strings
    
    * extract sellIn update to separate function
    
    * add tests for sellIn updates
    
    * implement updating for common items + aged brie
    
    * implement backstage passes
    
    * update each item kind separately
    
    * implement conjured and update snapshots
    
    * fix linter errors
    
    * dry clamping quality
    
    * add more info to the readme

--- 2023-02-01-ts/src/gilded-rose.test.ts
+++ 2023-02-01-ts/src/gilded-rose.test.ts
@@ -1,8 +1,229 @@
 import runGoldenMaster from "./test-data/golden-master";
+import { ItemKind, kindForItemName, Item, GildedRose } from "./gilded-rose";
 
 describe("Gilded Rose golden master tests", () => {
   it("Matches the golden master after 1 day", () => {
     const goldenMaster = runGoldenMaster(1);
     expect(goldenMaster).toMatchSnapshot();
+  });
+
+  it("Matches the golden master after 2 days", () => {
+    const goldenMaster = runGoldenMaster(2);
+    expect(goldenMaster).toMatchSnapshot();
+  });
+
+  it("Matches the golden master after 5 days", () => {
+    const goldenMaster = runGoldenMaster(5);
+    expect(goldenMaster).toMatchSnapshot();
+  });
+
+  it("Matches the golden master after 10 days", () => {
+    const goldenMaster = runGoldenMaster(10);
+    expect(goldenMaster).toMatchSnapshot();
+  });
+});
+
+describe("Item kinds", () => {
+  it("Returns `AgedBrie` for aged brie", () => {
+    const kind = kindForItemName("Aged Brie");
+    expect(kind).toBe(ItemKind.AgedBrie);
+  });
+
+  it("Returns `BackstagePass` for concert passes", () => {
+    const kind = kindForItemName("Backstage passes to a TAFKAL80ETC concert");
+    expect(kind).toBe(ItemKind.BackstagePass);
+  });
+
+  it("Returns `Legendary` for Sulfuras", () => {
+    const kind = kindForItemName("Sulfuras, Hand of Ragnaros");
+    const altKind = kindForItemName("Sulfuras");
+
+    expect(kind).toBe(ItemKind.Legendary);
+    expect(altKind).toBe(ItemKind.Legendary);
+  });
+
+  it("Returns `Conjured` for a conjured item", () => {
+    const kind = kindForItemName("Conjured sword");
+    expect(kind).toBe(ItemKind.Conjured);
+  });
+
+  it("Returns `Common` for misc items", () => {
+    const kind = kindForItemName("Lorem ipsum");
+    expect(kind).toBe(ItemKind.Common);
+  });
+});
+
+const anyItem = () => {
+  return new Item("A sword", 5, 10);
+};
+
+const anyLegendaryItem = () => {
+  return new Item("Sulfuras", 0, 80);
+};
+
+const anyCommonItem = (sellIn: number, quality: number) => {
+  return new Item("A sword", sellIn, quality);
+};
+
+const anyAgedBrie = (sellIn: number, quality: number) => {
+  return new Item("Aged Brie", sellIn, quality);
+};
+
+const anyBackstagePass = (sellIn: number, quality: number) => {
+  return new Item("Backstage pass", sellIn, quality);
+};
+
+const anyConjuredItem = (sellIn: number, quality: number) => {
+  return new Item("Conjured gizmo", sellIn, quality);
+};
+
+describe("GildedRose", () => {
+  describe("Sell In values", () => {
+    it("Decreases in each update", () => {
+      const gildedRose = new GildedRose([anyItem()]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.sellIn).toBe(anyItem().sellIn - 1);
+    });
+
+    it("Remains constant for legendary items", () => {
+      const gildedRose = new GildedRose([anyLegendaryItem()]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.sellIn).toBe(anyLegendaryItem().sellIn);
+    });
+  });
+
+  describe("Quality", () => {
+    it("Degrades for common items", () => {
+      const gildedRose = new GildedRose([anyCommonItem(1, 4)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(3);
+    });
+
+    it("Degrades twice as fast once the sell by date has passed", () => {
+      const gildedRose = new GildedRose([anyCommonItem(0, 4)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(2);
+    });
+
+    it("Never drops below zero", () => {
+      const gildedRose = new GildedRose([anyCommonItem(1, 0)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(0);
+    });
+  });
+
+  describe("Legendary item", () => {
+    it("Never has to be sold or decreases in quality", () => {
+      const gildedRose = new GildedRose([anyLegendaryItem()]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(anyLegendaryItem().quality);
+      expect(item.sellIn).toBe(anyLegendaryItem().sellIn);
+    });
+  });
+
+  describe("Aged Brie", () => {
+    it("Increases in quality the older it gets", () => {
+      const gildedRose = new GildedRose([anyAgedBrie(1, 10)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(11);
+    });
+
+    it("Increases in quality twice as fast when the sell by date has passed", () => {
+      const gildedRose = new GildedRose([anyAgedBrie(0, 10)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(12);
+    });
+
+    it("Never gets updated above 50", () => {
+      const gildedRose = new GildedRose([anyAgedBrie(1, 50)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(50);
+    });
+  });
+
+  describe("Backstage pass", () => {
+    it("Increases in quality by 1 when the concert approaches and 11+ days remain", () => {
+      const gildedRose = new GildedRose([anyBackstagePass(11, 20)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(21);
+    });
+
+    it("Increases in quality by 2 when 10 or less days remain", () => {
+      const gildedRose = new GildedRose([anyBackstagePass(10, 20)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(22);
+    });
+
+    it("Increases in quality by 3 when 5 or less days remain", () => {
+      const gildedRose = new GildedRose([anyBackstagePass(5, 20)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(23);
+    });
+
+    it("Drops to zero quality after the concert", () => {
+      const gildedRose = new GildedRose([anyBackstagePass(0, 20)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(0);
+    });
+
+    it("Never gets updated above 50 quality", () => {
+      const gildedRose = new GildedRose([anyBackstagePass(5, 49)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(50);
+    });
+  });
+
+  describe("Conjured item", () => {
+    it("Degrades twice as fast", () => {
+      const gildedRose = new GildedRose([anyConjuredItem(5, 10)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(8);
+    });
+
+    it("Degrades twice as fast when the sell by date has passed", () => {
+      const gildedRose = new GildedRose([anyConjuredItem(0, 10)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(6);
+    });
+
+    it("Never gets its quality drop below zero", () => {
+      const gildedRose = new GildedRose([anyConjuredItem(1, 0)]);
+
+      const [item, ..._] = gildedRose.updateQuality();
+
+      expect(item.quality).toBe(0);
+    });
   });
 });
--- 2023-02-01-ts/src/gilded-rose.ts
+++ 2023-02-01-ts/src/gilded-rose.ts
@@ -11,6 +11,7 @@ export class Item {
 }
 
 export class GildedRose {
+  static MAX_QUALITY = 50;
   items: Array<Item>;
 
   constructor(items = [] as Array<Item>) {
@@ -18,60 +19,106 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
-
+    this.items = this.items.map(updateQualityForItem);
     return this.items;
   }
+
+  static clampQuality(quality: number): number {
+    return Math.min(Math.max(0, quality), this.MAX_QUALITY);
+  }
+}
+
+export enum ItemKind {
+  AgedBrie = "Aged brie",
+  BackstagePass = "Backstage passes",
+  Legendary = "Legendary",
+  Common = "Common",
+  Conjured = "Conjured",
+}
+
+export function kindForItemName(name: string): ItemKind {
+  if (/^aged brie$/i.test(name)) {
+    return ItemKind.AgedBrie;
+  }
+
+  if (/^backstage pass(es)?/i.test(name)) {
+    return ItemKind.BackstagePass;
+  }
+
+  if (/^sulfuras([,\s]?.+|$)/i.test(name)) {
+    return ItemKind.Legendary;
+  }
+
+  if (/^conjured(\s.+)?/i.test(name)) {
+    return ItemKind.Conjured;
+  }
+
+  return ItemKind.Common;
+}
+
+export function updateQualityForItem(item: Item): Item {
+  const kind = kindForItemName(item.name);
+
+  switch (kind) {
+    case ItemKind.AgedBrie:
+      return updateAgedBrieItem(item);
+    case ItemKind.Legendary:
+      return updateLegendaryItem(item);
+    case ItemKind.BackstagePass:
+      return updateBackstagePassItem(item);
+    case ItemKind.Conjured:
+      return updateConjuredItem(item);
+    default:
+      return updateCommonItem(item);
+  }
+}
+
+function updateCommonItem(item: Item): Item {
+  const sellIn = item.sellIn - 1;
+
+  const qualityDelta = sellIn >= 0 ? -1 : -2;
+  const quality = Math.max(0, item.quality + qualityDelta);
+
+  return new Item(item.name, sellIn, quality);
+}
+
+function updateAgedBrieItem(item: Item): Item {
+  const sellIn = item.sellIn - 1;
+
+  const qualityDelta = sellIn >= 0 ? 1 : 2;
+  const quality = GildedRose.clampQuality(item.quality + qualityDelta);
+
+  return new Item(item.name, sellIn, quality);
+}
+
+function updateLegendaryItem(item: Item): Item {
+  return new Item(item.name, item.sellIn, item.quality);
+}
+
+function updateBackstagePassItem(item: Item): Item {
+  const qualityDelta = ((sellIn) => {
+    switch (true) {
+      case sellIn > 10:
+        return 1;
+      case sellIn > 5:
+        return 2;
+      case sellIn > 0:
+        return 3;
+      default:
+        return -item.quality;
+    }
+  })(item.sellIn);
+
+  const sellIn = item.sellIn - 1;
+  const quality = GildedRose.clampQuality(item.quality + qualityDelta);
+
+  return new Item(item.name, sellIn, quality);
+}
+
+function updateConjuredItem(item: Item): Item {
+  const sellIn = item.sellIn - 1;
+  const qualityDelta = sellIn >= 0 ? -2 : -4;
+  const quality = GildedRose.clampQuality(item.quality + qualityDelta);
+
+  return new Item(item.name, sellIn, quality);
 }
